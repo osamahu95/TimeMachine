d06a8f642042fe66606900ebcfea2cde18fc43b2
osamascience96 <osamahu96@gmail.com>
2025-04-24 19:03:29 +0500

    initial design of the schedule event page

---
+++ TImeMachine/Models/EventSchedule.cs
@@ -0,0 +1,11 @@
+﻿namespace TImeMachine.Models
+{
+    public class EventSchedule
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime UtcDateTime { get; set; }
+        public string  OriginalTimeZone { get; set; }
+    }
+}
